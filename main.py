44a4f3295320c4a4a463dbeef339a0f8a50d0ae6
Mryuvi1 <137717168+Mryuvi1@users.noreply.github.com>
2025-07-05 12:08:51 +0530

    Update main.py

--- main.py
+++ main.py
@@ -18,8 +18,6 @@ def get_ip():
         return "Unavailable"
 
 if __name__ == '__main__':
-    app.run(debug=True)
-if __name__ == '__main__':
     import os
-    port = int(os.environ.get("PORT", 5000))
+    port = int(os.environ.get("PORT", 10000))
     app.run(host='0.0.0.0', port=port)
