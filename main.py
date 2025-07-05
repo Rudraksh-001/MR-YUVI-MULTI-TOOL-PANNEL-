9a285794f63d9088859b3ffbfe83f14693e6bd9c
Mryuvi1 <137717168+Mryuvi1@users.noreply.github.com>
2025-07-05 11:57:11 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,22 @@
+
+from flask import Flask, render_template
+import datetime
+import requests
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    current_time = datetime.datetime.now()
+    ip = get_ip()
+    return render_template('index.html', time=current_time.strftime("%H:%M:%S"), date=current_time.strftime("%A, %d %B %Y"), ip=ip)
+
+def get_ip():
+    try:
+        response = requests.get("https://api.ipify.org?format=json")
+        return response.json().get("ip", "Unavailable")
+    except:
+        return "Unavailable"
+
+if __name__ == '__main__':
+    app.run(debug=True)
