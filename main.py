895b2e8fca5a397df61c3b58863b2d8111a88429
Mryuvi1 <137717168+Mryuvi1@users.noreply.github.com>
2025-07-05 12:06:25 +0530

    Update main.py

--- main.py
+++ main.py
@@ -2,7 +2,6 @@
 from flask import Flask, render_template
 import datetime
 import requests
-
 app = Flask(__name__)
 
 @app.route('/')
@@ -20,3 +19,7 @@ def get_ip():
 
 if __name__ == '__main__':
     app.run(debug=True)
+if __name__ == '__main__':
+    import os
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
